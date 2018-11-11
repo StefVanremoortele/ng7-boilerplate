cbc4b95ad574923c62f9dc62da575c89550ab21a
Stef Vanremoortele <stefvanremoortele@gmail.com>
2018-11-11 01:32:25 +0100

    init

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component, ViewChild } from '@angular/core';
+import { MatSidenav } from '@angular/material';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  @ViewChild('sidenav') public sidenav: MatSidenav;
+  isDarkTheme = false;
+
+  title = 'ng7-pre';
+}
+  
